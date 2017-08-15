afe17d423ea87edbd6a8ec89b8ce3124a269a01f
Kyle Moser <KMoser@TecniplastUSA.int>
2017-08-15 15:47:30 -0400

    tweaking file work
    played with writing files

--- filework.py
+++ filework.py
@@ -1,6 +1,6 @@
 
 # Playing with files
-f=open('test.txt')
+f = open('test.txt','r+')     # the r+ is read/write mode
 print 'Output of read'
 print f.read()
 print
@@ -12,3 +12,9 @@ print 'iterating through file with for loop'
 for stuff in open('test.txt'):
     print stuff
 print
+for i in range(2):                              # Iterate through writing lines
+    f.write("This is line %s\n" % (i+1))
+f.close()
+f = open('test.txt')
+print f.read()
+f.close()
