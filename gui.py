a0ecd00e0843348814e2837e2c59d3fb28c02344
KCMoser <email.kylemoser@gmail.com>
2017-09-16 11:13:58 -0400

    GUI updates

--- gui.py
+++ gui.py
@@ -1,15 +1,15 @@
 # @author:Kyle Moser
-from tkinter import*                       #GUI module import
-class App(Frame):                          #Defines class called App based on Frame class
-    def__init__(self,master):              #Define constructor method
-        super(App, self).__init__(master)  #Use frame attributes and methods & frame's master is root window
-#Define variables, storage locations
-#Define widgets
-#--End of Constructor--
+from tkinter import*                #GUI module import
 
-# Main
-root=Tk()                             #Build standard window object called root
-root.title("Aligned Focus")                #Assign title to title bar
-root.geometry("400x600")                   #Set window dimensions
-app=App(root)        #Create object called app based on App class defined above and pass to root
-root.mainloop()                        #Launch window and start event listening
+def show_button_action():           #Action for button press created
+    #print('button has been pressed')#Do something
+    response.print('button has been pressed')
+
+root=Tk()                           #Build standard window object called root
+root.title("Aligned Focus")         #Assign title to title bar
+root.geometry("600x400")            #Set window dimensions
+genericText=Label(root,text='This is a label for the window').grid(row=1,column=3,padx=5,pady=5) #Generic text for window
+buttonOne=Button(root,text='button text',command=show_button_action).grid(row=2,column=1,padx=5,pady=5) #button with action
+response=Label(root,text='start').grid(row=3,column=3,padx=5,pady=5) #set area for button press to show
+               
+root.mainloop()                     #Launch window and start event listening
