0ceac6e8c7e50cf98dd94920f65b02694a71a6f9
KCMoser <email.kylemoser@gmail.com>
2017-10-12 12:15:27 -0400

    reference of key

---
+++ read_in_file_line_to_variable.py
@@ -0,0 +1,4 @@
+f = open('access_token.txt','r+')       # Open access_token file for read/write mode
+access_token=f.read()                   # Read contents of file into variable called access_token
+print ('Output of read:')
+print (access_token)                    # Prints access code
