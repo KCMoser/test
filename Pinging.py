036de3c5eb573326d55aae6b75ea73026a6dccb7
KCMoser <email.kylemoser@gmail.com>
2017-10-27 13:32:33 -0400

    app cleanup

--- Pinging.py
+++ Pinging.py
@@ -2,7 +2,6 @@ import sys                                  # For grabbing OS version
 import subprocess
 
 os_name=sys.platform                        # Assign OS to variable
-from os import system as system_call        # Execute a shell command
 
 host='10.11.32.94'                        # Assigned for testing
 
@@ -11,14 +10,10 @@ if os_name=="win32":
     options=str('-n 1')
 else:
     options=str('-c 1')
-#print(options)                              # For testing
 
     # Pinging
-#subprocess.run(['ping', host, options], stdout=subprocess.PIPE)
-#print('ping '+host+ ' ' +options)
 pingresult=subprocess.getoutput('ping' +' '+ host+' '+ options)
 print(pingresult)
-
 if 'unreachable' in pingresult:
     print(host+ " is Offline")
 elif 'Request' in pingresult:
