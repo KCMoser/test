bf09e64b5d8acff3489c48bb2d659470ed814987
KCMoser <email.kylemoser@gmail.com>
2017-11-11 09:00:02 -0500

    tweaks to versioning

--- version checking.py
+++ version checking.py
@@ -1,4 +1,5 @@
 import sys
+import platform
 import logging
 
 # Set up timestamp and logfile name...
@@ -6,6 +7,7 @@ logging.basicConfig(filename='results.log', level=logging.INFO, format='%(asctim
 # Check Python and OS version...
 if sys.version_info[0] < 3:
     print ('This program requires Python version 3')
-else: print ('Current version is '+sys.version)
-logging.info('Python version is ' + sys.version)
-logging.info('Operating system is: '+ sys.platform)
+else: print ('Current version is '+sys.version[0]+sys.version[1] +sys.version[2])
+print ('Operating system is: '+platform.system()+' '+platform.release())
+logging.info('Python version is ' +sys.version[0]+sys.version[1] +sys.version[2])
+logging.info('Operating system is: '+platform.system()+' '+platform.release())
